40685d48ff4ae2727b1b7bcff78435305621682d
Blair Motchan <blair.motchan@powerreviews.com>
2024-11-27 12:05:46 -0500

    get me

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,15 +1,37 @@
+"use client";
 import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
+import { useFormState } from "react-dom";
+import login from "./login";
 
 export default function Login() {
+  const [state, formAction] = useFormState(login, { error: "" });
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" variant="outlined" type="email" />
-      <TextField label="Password" variant="outlined" type="password" />
-      <Button variant="contained">Login</Button>
-      <Link component={NextLink} href="/auth/signup" className="self-center">
-        Signup
-      </Link>
-    </Stack>
+    <form action={formAction}>
+      <Stack spacing={2} className="w-full max-w-xs">
+        <TextField
+          error={!!state.error}
+          helperText={state.error}
+          label="Email"
+          variant="outlined"
+          type="email"
+          name="email"
+        />
+        <TextField
+          error={!!state.error}
+          helperText={state.error}
+          label="Password"
+          variant="outlined"
+          type="password"
+          name="password"
+        />
+        <Button variant="contained" type="submit">
+          Login
+        </Button>
+        <Link component={NextLink} href="/auth/signup" className="self-center">
+          Signup
+        </Link>
+      </Stack>
+    </form>
   );
 }
