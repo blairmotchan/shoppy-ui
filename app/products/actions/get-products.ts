223ea593f434d72b5c5b73221451b95269fb4697
Blair Motchan <blair.motchan@powerreviews.com>
2024-12-03 15:54:41 -0500

    small changes

--- app/products/actions/get-products.ts
+++ app/products/actions/get-products.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { get } from "@/app/common/util/fetch";
-import Product from "../product";
 
 export default async function getProducts() {
   return get<Product[]>("products");
--- app/products/create-product/create-product-modal.tsx
+++ app/products/create-product/create-product-modal.tsx
@@ -3,7 +3,7 @@
 import { Box, Modal } from "@mui/material";
 import { Stack, Button, Link, TextField } from "@mui/material";
 import { useState } from "react";
-import { FormResponse } from "../common/interfaces/form-response.interface";
+import { FormResponse } from "@/app/common/interfaces/form-response.interface";
 import createProduct from "../actions/create-product";
 
 const styles = {
